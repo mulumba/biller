3912f4774f09544e50cbcb96921b3c68a8cfeab3
Mulumba Lwatula <mulumba@mn8.co>
2014-07-28 00:28:59 -0400

    Project model created and validation added

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,15 @@
 class Account < ActiveRecord::Base
   has_many :account_entries
   
-  validates :name, presence: true, length: {in: 1..70}
+  validates :name, presence: true, 
+                   length: {in: 1..70,
+                            message: "Please use a decent name."}, 
+                   uniqueness: true
+  validate :your_name_is_not_dumb
+  
+  def your_name_is_not_dumb
+    if name.include?("Dumb")
+       errors.add(:name, "is Dumb")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727234040) do
+ActiveRecord::Schema.define(version: 20140728035421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20140727234040) do
     t.string   "name"
     t.string   "email"
     t.string   "about"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.integer  "customer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
